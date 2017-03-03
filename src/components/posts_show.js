c391139d258466abbece29c7346ac79efc870789
Vivek Gondalia <vivekgondalia@Viveks-MBP.home>
2017-03-03 00:40:05 -0500

    Added Back to Home Feature from Selcted Post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
-
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount(){
@@ -20,6 +20,8 @@ class PostsShow extends Component {
         <h3>{ post.title }</h3>
         <h6>Categories: { post.categories}</h6>
         <p>{ post.content }</p>
+
+        <Link to="/" className="btn btn-primary">Back To Home</Link>
       </div>
     );
   }
