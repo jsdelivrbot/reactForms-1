fb186507975d31a97bef25b06ab6fe6e51e9910e
Vivek Gondalia <vivekgondalia@Viveks-MBP.home>
2017-03-03 00:36:24 -0500

    Display selected post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,11 +9,24 @@ class PostsShow extends Component {
   }
 
   render(){
+    const post = this.props.post;
+
+    if(!post){
+      return <div>Loading...</div>;
+    }
+
     return(
-      <div> Show post {this.props.params.id}</div>
+      <div>
+        <h3>{ post.title }</h3>
+        <h6>Categories: { post.categories}</h6>
+        <p>{ post.content }</p>
+      </div>
     );
   }
 }
 
+function mapStateToProps(state){
+  return { post: state.posts.post };
+}
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
