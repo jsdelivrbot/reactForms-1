fc16bbd98597f49c944a48022589c9a6137d8d57
Vivek Gondalia <vivekgondalia@Viveks-MBP.home>
2017-03-03 00:23:17 -0500

    Action Craetor to fetch single post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+
 
 class PostsShow extends Component {
+  componentWillMount(){
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render(){
     return(
       <div> Show post {this.props.params.id}</div>
@@ -8,4 +15,5 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+
+export default connect(null, { fetchPost })(PostsShow);
